2a9e22ed680d333969f187294894c3eb902283bc
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2023-11-21 21:04:01 +0800

    1. favouite button works.

--- src/components/FavouritesList/FavouritesList.jsx
+++ src/components/FavouritesList/FavouritesList.jsx
@@ -3,34 +3,7 @@ import FavouritesListItem from "../FavouriteListItem/FavouriteListItem";
 
 import './FavouritesList.css'
 
-function FavouritesList() {
-
-  const [favSpaceData, setFavSpaceData] = useState([]);
-
-  //airtable fetch
-  const TOKEN = import.meta.env.VITE_AIRTABLE_TOKEN;
-  // need to update airtable table ID;
-  const BASE_URL = "https://api.airtable.com/v0/appFFEGBwUf8bQvIk"
-
-  useEffect(() => {
-    async function fetchFavSpaces() {
-
-      const response = await fetch(`${BASE_URL}/favSpaceAT`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${TOKEN}`,
-        },
-      });
-      const jsonData = await response.json();
-      const spaceData = jsonData.records.map((data) => ({
-        ...data.fields,
-        id: data.id,
-      }));
-      console.log(spaceData);
-      setFavSpaceData(spaceData);
-    }
-    fetchFavSpaces();
-  }, []);
+function FavouritesList({ favSpaceData }) {
 
     return (
       <>
--- src/components/ResultSummary/ResultSummary.jsx
+++ src/components/ResultSummary/ResultSummary.jsx
@@ -1,12 +1,26 @@
-function ResultSummary({ selectedItem }) {
+import { useState } from "react";
+
+function ResultSummary({ selectedItem, addToFavorites }) {
+
   if (!selectedItem) {
-    return <div> </div>;
+    return <div></div>;
   }
 
   return (
     <div className="result-summary">
       <img src={selectedItem.links[0].href} alt={selectedItem.data[0].title} />
       <h2>{selectedItem.data[0].title}</h2>
+      <p>{selectedItem.data[0].description}</p>
+      <button
+        className="favorite-button"
+        onClick={() => addToFavorites({
+          title: selectedItem.data[0].title,
+          image: selectedItem.links[0].href,
+          comment: selectedItem.data[0].comment
+        })}
+      >
+        Add to Favorites
+      </button>
     </div>
   );
 }
