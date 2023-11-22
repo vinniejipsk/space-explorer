88b79684d761f0b8a2daeb69b23490095a0106cd
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2023-11-23 00:27:31 +0800

    1. video plays. video sorting.

--- src/pages/Videos/Videos.jsx
+++ src/pages/Videos/Videos.jsx
@@ -7,37 +7,74 @@ import './Videos.css';
 
 function Videos() {
 
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
+  const [sortMethod, setSortMethod] = useState('recent');
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videoUrl, setVideoUrl] = useState('');
 
-    async function VideosApiCall() {
-      
-        const response = await fetch('https://images-api.nasa.gov/search?media_type=video');
-        const data = await response.json();
-        setVideos(data.collection.items);
+  async function VideosApiCall() {
+    const response = await fetch('https://images-api.nasa.gov/search?media_type=video');
+    const data = await response.json();
+    setVideos(data.collection.items);
+  }
+  useEffect(() => {
+    VideosApiCall();
+  }, []);
+
+  async function fetchVideoUrl(nasaId) {
+    const response = await fetch(`https://images-api.nasa.gov/asset/${nasaId}`);
+    const data = await response.json();
+    const videoFile = data.collection.items.find(item => item.href.endsWith('.mp4'));
+    if (videoFile) {
+        setVideoUrl(videoFile.href);
     }
-    
-    useEffect(() => {
-      VideosApiCall();
-    }, []);
+  }
 
-      return (
-        <>
+  function handleThumbnailClick(video) {
+    console.log("Thumbnail clicked", video);
+    const nasaId = video.data[0].nasa_id;
+    fetchVideoUrl(nasaId);
+    setSelectedVideo(video);
+  }
 
-          <Navigation />
+  function sortVideos(videos, method) {
+    if (method === 'new') {
+      return [...videos].sort((a, b) => new Date(b.data[0].date_created) - new Date(a.data[0].date_created));
+    } else if (method === 'old') {
+      return [...videos].sort((a, b) => new Date(a.data[0].date_created) - new Date(b.data[0].date_created));
+    }
+    return videos;
+  }
 
-          <div className="videos-container">
-            {videos.map((video, index) => (
-              <div className="video-item" key={index}>
-                <VideoItem
-                  // title={video.data[0].title}
-                  thumbnailUrl={video.links[0].href}
-                />
-              </div>
-            ))}
+  return (
+      <>
+        <Navigation />
+        {selectedVideo && (
+          <video 
+            key={videoUrl} 
+            width="720" 
+            height="480" 
+            controls 
+            autoPlay 
+            muted
+          >
+            <source src={videoUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+        <div className="sort-buttons">
+          <button onClick={() => setSortMethod('new')}>Recent</button>
+          <button onClick={() => setSortMethod('old')}>Oldest</button>
+        </div>
+        <div className="videos-container">
+        {sortVideos(videos, sortMethod).map((video, index) => (
+          <div className="video-item" key={index} onClick={() => handleThumbnailClick(video)}>
+              <VideoItem thumbnailUrl={video.links[0].href} />
           </div>
-          
-        </>
-    );
+        ))}
+        </div>
+      </>
+  );
 }
 
-export default Videos
+export default Videos;
