c6bde03be0d21e688e312f0ba3c097a0f7770ac3
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2023-11-24 21:59:50 +0800

    1. renamed route links

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './Pages/Home/Home';
-import Apod from './Pages/Apod/Apod';
-import Videos from './Pages/Videos/Videos';
+import Home from './pages/Home/Home';
+import Apod from './pages/Apod/Apod';
+import Videos from './pages/Videos/Videos';
 
 import './index.css';
 
