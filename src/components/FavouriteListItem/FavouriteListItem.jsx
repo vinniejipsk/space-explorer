a369e5144c105f880ae831aba4a3f710d5d524a7
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2023-11-24 21:38:14 +0800

    1. Added update logic. updated css.

--- src/components/FavouriteListItem/FavouriteListItem.jsx
+++ src/components/FavouriteListItem/FavouriteListItem.jsx
@@ -1,19 +1,50 @@
-function FavouritesListItem({ title, image, comment, onDelete  }) {
+import { useState } from "react";
+
+function FavouritesListItem({ title, image, comment, onDelete, onUpdate  }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedComment, setEditedComment] = useState(comment);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleUpdate = () => {
+    onUpdate(editedComment);
+    setIsEditing(false);
+  };
 
   return (
     <div className="fav-item">
       {image && <img src={image} alt={title} className="fav-img" />}
-      <div className= "fav-text">
+      <div className="fav-text">
         <h3>{title}</h3>
-        <p>{comment}</p>
-        <button 
-          onClick={onDelete}
-          className="delete-button"
-        >
-          Delete</button>
+        <div className= "edit-delete-container">
+          {isEditing ? (
+            <input
+              type="text"
+              value={editedComment}
+              onChange={(e) => setEditedComment(e.target.value)}
+            />
+          ) : (
+            <p>{comment}</p>
+          )}
+          {isEditing ? (
+            <button onClick={handleUpdate} className="update-button">
+              Update
+            </button>
+          ) : (
+            <button onClick={handleEdit} className="edit-button">
+              Edit
+            </button>
+          )}
+          <button onClick={onDelete} className="delete-button">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
-  export default FavouritesListItem;
+export default FavouritesListItem;
